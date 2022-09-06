c730717699f2f03d14e4140aa46c0641a4b9abf4
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-09-06 20:42:00 +0900

    :sparkles: ICoodinatorインターフェースを追加

---
+++ Coodinators/ICoodinator.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace Coodinators
+{
+    public interface ICoodinator
+    {
+        public ICoodinator[] ChildCoodinators { get; set; }
+        public UINavigationController NavigationController { get; set; }
+
+        public void Start();
+    }
+}
