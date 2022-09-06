ee3721f02bff2d4c8e4dc5c806d6f856e725ac66
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-09-07 02:00:49 +0900

    :art: ICoodinatedViewControllerインターフェースの定義を変更

--- Coodinators/ICoodinatedViewController.cs
+++ Coodinators/ICoodinatedViewController.cs
@@ -4,6 +4,21 @@ namespace Coodinators
 {
     public interface ICoodinatedViewController
     {
-        public WeakReference<MainCoodinator>? Coodinator { get; set; }
+        protected WeakReference<MainCoodinator>? _weakCoodinator { get; set; }
+
+        public sealed MainCoodinator Coodinator
+        {
+            get
+            {
+                if (_weakCoodinator is null)
+                    throw new NullReferenceException($"{nameof(_weakCoodinator)}が設定されていません。");
+
+                if (_weakCoodinator.TryGetTarget(out MainCoodinator coodinator) is false)
+                    throw new NullReferenceException($"{nameof(_weakCoodinator)}が設定されていません。");
+
+                return coodinator;
+            }
+            set => _weakCoodinator = new WeakReference<MainCoodinator>(value);
+        }
     }
 }
