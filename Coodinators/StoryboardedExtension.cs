ff47ddd4c3e186a77dca323a04e0e4fec33734ed
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-09-06 21:43:01 +0900

    :sparkles: StoryboardedExtensionクラスを追加

---
+++ Coodinators/StoryboardedExtension.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Coodinators
+{
+    public class StoryboardedExtension<Self> : IStoryboarded<Self> where Self : UIViewController
+    {
+        public static Self Instantiate()
+        {
+            string id = typeof(Self).Name;
+            UIStoryboard storyboard = UIStoryboard.FromName(
+                name: "Main",
+                storyboardBundleOrNil: NSBundle.MainBundle);
+
+            return (Self)storyboard.InstantiateViewController(identifier: id);
+        }
+    }
+}
