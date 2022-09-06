c37921471a1d236d670ddd127ba630f8b9863165
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-09-07 00:24:02 +0900

    :art: ViewControllerのボタンにタップ時のアクションを追加

--- Coodinators/ViewController.designer.cs
+++ Coodinators/ViewController.designer.cs
@@ -1,18 +1,25 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace Coodinators
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("FirstViewControllerTapped:")]
+		partial void FirstViewControllerTapped (Foundation.NSObject sender);
+
+		[Action ("SecondViewControllerTapped:")]
+		partial void SecondViewControllerTapped (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
 }
