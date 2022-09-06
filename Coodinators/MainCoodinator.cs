89f4fb3382d40f6a7ec2d41b504c4b64fd21ac8c
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2022-09-06 23:38:18 +0900

    :sparkles: MainCoodinatorクラスを追加

---
+++ Coodinators/MainCoodinator.cs
@@ -0,0 +1,21 @@
+﻿using UIKit;
+
+namespace Coodinators
+{
+    public class MainCoodinator : ICoodinator
+    {
+        public ICoodinator[] ChildCoodinators { get; set; } = new ICoodinator[] { };
+        public UINavigationController NavigationController { get; set; }
+
+        public MainCoodinator(UINavigationController navigationController)
+        {
+            NavigationController = navigationController;
+        }
+
+        public void Start()
+        {
+            UIViewController vc = StoryboardedExtension<ViewController>.Instantiate();
+            NavigationController.PushViewController(vc, animated: false);
+        }
+    }
+}
